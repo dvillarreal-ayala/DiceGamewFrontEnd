2a8be8180c32174355365a523ae98fe2b698f2c9
damianayala <damian.villarreal-ayala@menloschool.org>
2023-02-08 14:59:11 -0800

    Added Contributors

--- src/Die.java
+++ src/Die.java
@@ -1,2 +1,56 @@
-package PACKAGE_NAME;public class Die {
+public class Die {
+    /** Instance Variables **/
+    private int sides;
+
+    /** Constructors **/
+    public Die() {
+        sides = 6;
+    }
+
+    public Die(int numSides) {
+        sides = numSides;
+    }
+
+    /** Methods **/
+    public int getSides() {
+        return sides;
+    }
+
+    /**
+     * Returns a random int between 1 and
+     * the number of sides on the Die
+     */
+    public int roll() {
+        int num = (int)(Math.random()*sides) + 1;
+        return num;
+    }
+
+    /**
+     * Rolls the dice the numRolls times
+     * and returns the max value of the rolls
+     */
+    public int getMaxRoll(int numRolls) {
+        int max = Integer.MIN_VALUE;
+        for(int i=0; i<numRolls; i++) {
+            int num = roll();
+            max = Math.max(max, num);
+        }
+
+        return max;
+    }
+
+    /**
+     * Rolls to get a number between 3 and 12 for the round goal
+     * returns an int
+     */
+    public static int roundGoal() {
+        int num = (int)(Math.random() * 10) + 3;
+        return num;
+    }
+
+    /**
+     */
+    public String toString() {
+        return "This is a " + sides + " sided die.";
+    }
 }
--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class Game {
+/**
+ * Created by Damian Villarreal-Ayala @ Menlo School AP CS2 on Feb 8, 2023.
+ *
+ * Contributors: Elijah Chandler & Ozymandias, King of Cats.
+ */
+
+import java.util.Scanner;
+// Create methods within DieTester
+public class Game
+{
+    // Instance Variables 
+    //Use die for both players
+    private Die Die1;
+    private Die Die2;
+
+
+    //Constructor
+    public Game()
+    {
+        Die1 = new Die();
+        Die2 = new Die();
+    }
+    //method used to calculate and print winner of game.
+    public static String printWinner(int sum1, int sum2)
+    {
+        // Prints the winner based off of the sum of both players.
+        if (sum1 > sum2)
+        {
+            return "You won by " + (sum1 - sum2) + " points! Good game!";
+        }
+        else if (sum1 < sum2)
+        {
+            return "The computer won by " + (sum2 - sum1) + " points! Good game!";
+        }
+        return "It's a tie! Good game!";
+    }
+    //Print Intro and rules.
+    public static void printIntro()
+    {
+        System.out.println("The goal of each round is to get the target number, and each time you get the target number you get one point.");
+    }
+
+    public static void main(String[] args)
+    {
+        Game.printIntro();
+    }
 }
