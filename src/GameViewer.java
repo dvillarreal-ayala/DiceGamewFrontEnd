9990bf30fd939f2511dfe206e903a17d22462a74
damianayala <damian.villarreal-ayala@menloschool.org>
2023-02-14 01:11:37 -0800

    Finished Product

--- src/GameViewer.java
+++ src/GameViewer.java
@@ -5,14 +5,13 @@ public class GameViewer extends JFrame{
     private Player player1, player2;
     private final int WINDOW_WIDTH = 1000;
     private final int WINDOW_HEIGHT = 800;
-    private Image background, casino;
+    private Image background;
     public GameViewer(Game game)
     {
         this.game = game;
         player1 = game.getPlayer1();
         player2 = game.getPlayer2();
         this.background = new ImageIcon("Resources/Background.png").getImage();
-        this.casino = new ImageIcon("Resources/casino.jpeg").getImage();
 
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setTitle("Game Screen");
